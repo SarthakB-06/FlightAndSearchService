ae10904e68b5d126a444a82cc8dc54680a4cb4e8
Sarthak Bansal <SarthakBansal725@gmail.com>
2025-07-03 10:47:27 +0530

    added basic specs of the database sql

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+const app = express()
+
+dotenv.config();
+
+
+app.use(express.json());    
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+})
